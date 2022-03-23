3bf7f0ff6cb8512aa4c8f40702146a8b02ddfa71
nebr0k <90468237+nebr0k@users.noreply.github.com>
2022-03-23 19:57:05 +0200

    Add files via upload

---
+++ 8.1.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* str)
+{
+	int k = 0;
+	for (int i = 0; str[i + 1] != 0; i++)
+		if ( str[i] == 'a' && str[i+1] == 'b' && str[i+2] == 'c')
+		{
+			k++;	
+		}
+			return k;
+}
+char* Change(char* str)
+{
+	for (int i = 0; str[i + 1] != 0; i++)
+		if (str[i] == 'a' && str[i + 1] == 'b' && str[i + 2] == 'c')
+		{
+			
+			str[i] = ' ';
+			str[i+1] = '*';
+			str[i+2] = '*';
+		}
+	return str;
+}
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "Number of occurrences " << Count(str) <<  endl;
+	char* dest = new char[151];
+	dest = Change(str);
+	cout << "Modified string : " << dest << endl;
+	return 0;
+}
